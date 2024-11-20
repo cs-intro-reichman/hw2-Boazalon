3ea11c84f9bec5cec69483289a382b250a7f5279
בועז אלון <boaz@MacBook-Air-sl-bwz.local>
2024-11-20 16:39:00 +0200

    1

--- CalcPi.java
+++ CalcPi.java
@@ -2,5 +2,24 @@
 public class CalcPi {
 	public static void main(String [] args) { 
 	    // Replace this comment with your code
+		double approximated = 1;
+		double denominator = 3;
+
+		for (int i = 1 ; i < Integer.parseInt(args[0]); i++ ){
+
+			if (i%2 != 0) {
+
+				approximated = approximated - (1/denominator) ;
+				
+			}
+			else {
+				approximated = approximated + (1/denominator) ;
+			}
+			denominator = denominator + 2;
+		}
+
+
+		System.out.println("pi according to Java: " + Math.PI);
+		System.out.println("pi, approximated: " + 4 * approximated);
 	}
 }
--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,38 @@
 public class TimeCalc {
     public static void main(String[] args) {
-        // Replace this comment with your code
+
+        int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
+
+		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
+        
+		int moreminutes = Integer.parseInt(args[1]);
+		
+
+        int Totalminutes = ( hours * 60) + minutes + moreminutes;
+
+        int Totalhours = Totalminutes / 60;
+        
+        int Newhours = Totalhours % 24; 
+        
+        int Newminutes = Totalminutes - (Totalhours * 60);
+
+        
+        if (Newhours<10) {
+            System.out.print("0" + Newhours );
+            
+        }
+        else {
+            System.out.print( Newhours );
+        }
+        if (Newminutes<10) {
+            System.out.print( ":0" + Newminutes );
+            
+        }
+        else {
+            System.out.print(":" + Newminutes );
+        }
+
+        
+
     }
 }
