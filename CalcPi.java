b4600bc67b50dfd1045d3e66a255f98280035252
בועז אלון <boaz@MacBook-Air-sl-bwz.local>
2024-11-20 16:47:17 +0200

    2

--- CalcPi.java
+++ CalcPi.java
@@ -20,6 +20,6 @@ public class CalcPi {
 
 
 		System.out.println("pi according to Java: " + Math.PI);
-		System.out.println("pi, approximated: " + 4 * approximated);
+		System.out.println("pi, approximated:     " + 4 * approximated);
 	}
 }
